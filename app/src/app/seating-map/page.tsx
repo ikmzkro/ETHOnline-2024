eeff2b873c64f171507fa138d928c8f559e81c6b
ikmz <ikmz0104.official@gmail.com>
2024-08-28 23:27:16 +0900

    feat: url routing

---
+++ app/src/app/seating-map/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function SplitterCreation() {
+  const router = useRouter();
+
+  return (
+    <>
+      <p>sasasasas</p>
+    </>
+  );
+}
