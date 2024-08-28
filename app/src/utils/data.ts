57c1207e0cce284289b40ae93de8e9e28a51689a
ikmz <ikmz0104.official@gmail.com>
2024-08-28 21:38:59 +0900

    put adi

---
+++ app/src/utils/data.ts
@@ -0,0 +1,16 @@
+export const matches = [
+  {
+    id: "jwc-2025-1",
+    teams: "Vissel Kobe vs Tottenham Hotspur FC",
+    kickoff: "19:00",
+    homeLogo: "/kobe.png",
+    awayLogo: "/spurs.png",
+    buttonsDisabled: false,
+    stadiumName: "Japan National Stadium",
+    entryGate: "Gate A",
+    matchDate: "2025-08-01",
+    openingTime: "18:00",
+    kickoffTime: "19:00",
+    contributionPool: "1250",
+  },
+];
