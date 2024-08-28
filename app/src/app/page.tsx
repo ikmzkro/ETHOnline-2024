3c03a03d53e8f1f2bcbbb73e3397ae201a328030
ikmz <ikmz0104.official@gmail.com>
2024-08-28 21:28:59 +0900

    frontend build

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,17 +1,11 @@
 import { HeaderComponent } from "@/components/header-component";
 import { FooterComponent } from "@/components/footer-component";
-import { useWallet } from "@/contexts/WalletContext";
 
 export default function Home() {
-  const { account, signer, connectWallet, disConnectWallet } = useWallet();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <>
-        <HeaderComponent
-          account={account}
-          connectWallet={connectWallet}
-          disConnectWallet={disConnectWallet}
-        />
+        {/* <HeaderComponent /> */}
         <FooterComponent />
       </>
     </main>
