3589a5090bbefde60673c661e4db056ae1c6f6a3
ikmz <ikmz0104.official@gmail.com>
2024-08-28 21:16:31 +0900

    feat: useContext signer obj

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,18 @@
-import { HomePage } from "@/components/home-page";
+import { HeaderComponent } from "@/components/header-component";
+import { FooterComponent } from "@/components/footer-component";
+import { useWallet } from "@/contexts/WalletContext";
 
 export default function Home() {
+  const { account, signer, connectWallet, disConnectWallet } = useWallet();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <>
-        <HomePage />
+        <HeaderComponent
+          account={account}
+          connectWallet={connectWallet}
+          disConnectWallet={disConnectWallet}
+        />
+        <FooterComponent />
       </>
     </main>
   );
